be5c26b99ffe4a792dc3e842aa90048d17a265ce
[U-dias] <[nomd_adniss@yahoo.co.jp]>
2022-12-09 11:46:47 +0000

    ver.2

--- calc.js
+++ calc.js
@@ -1,17 +1,66 @@
-function update(_v){
-     document.querySelector('input').value = _v
-   }
-   function append(_v){
-     document.querySelector('input').value += _v
-   }
-   function calc(){
-        const v = document.querySelector( 'input' ).value
-        try{
-        const f = new Function( 'return ' + v )
-        update( f().toString() )}
-        catch( _error){
-          update( _error)
-        }
+//計算結果//
+var result="";
+//=で計算したかどうか//
+var is_calc=false;
+
+//初期表示//
+window.onload=function(){
+    result=document.getElementById('result');
+};
+
+//「Clear」キー入力//
+function c_click(){
+    result.value="0";//計算結果に０を表示//
+    is_calc=false;
+};
+
+//数字キー入力//
+function num_click(val){
+  if(is_calc)  result.value = "0";
+  is_calc = false;  
+
+  if(result.value =="0" && val == "0"){//計算結果が０の時にvalに０を代入
+    result.value = "0";//０を返す
+  }else if(result.value == "0" && val == "."){//計算結果が０の時に.を入力
+    result.value = "0.";//0.を返す
+  }else if(result.value == "0"){//0以外を代入
+    result.value = val;//入力された値を返す
+  }else{
+    result.value += val;
+  }
+}
+
+//演算子キー入力//
+function ope_click(val){
+  if(is_calc)  is_calc = false;
+  
+  if(is_ope_last()){
+    result.value = result.value.slice(0, -1) + val;
+    //sliceの引数で開始位置より終了位置の値を小さくして空文字を返す+入力された値を表示
+  } else {
+    result.value += val;
+    //入力された値を加えて代入
+  }
+}
+
+//イコールキー入力//
+function equal_click(){
+  if(is_ope_last())  result.value = result.value.slice(0, -1);
+//演算子キーが最後にある場合sliceの引数で除外
+  var temp = new Function("return " + result.value.replaceAll("×", "*").replaceAll("÷", "/"))();
+//tempに計算結果を代入＋演算子をjavascript上で動くように置き換え
+  if(temp == Infinity || Number.isNaN(temp)){
+    result.value = "Error";
+//計算結果に無限数と数字以外の場合error表示
+  }else{
+    result.value = temp;
+    is_calc = true;
+  }
+}
+
+//入力された値が演算子かどうかの判定//
+function is_ope_last(){
+  return ["+","-","×","÷","%"].includes(result.value.toString().slice(-1));
 }
 
 
